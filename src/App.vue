c54019a0769badeff5aa12f603e6856aa02d58a9
ISC-EIbarra <EdsonP4100@gmail.com>
2024-03-11 18:03:54 -0700

    Add dynamic products

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 <script setup>
 import { ref, reactive, onMounted } from 'vue';
 import { db } from './data/guitarras.js';
+import Guitarra from './components/Guitarra.vue';
 
 // const state = reactive({
 //   guitarras: [],
@@ -115,26 +116,7 @@ console.log(guitarras.value);
     <h2 class="text-center">Nuestra Colección</h2>
 
     <div class="row mt-5">
-      <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-        <div class="col-4">
-          <img
-            class="img-fluid"
-            src="/img/guitarra_01.jpg"
-            alt="imagen guitarra"
-          />
-        </div>
-        <div class="col-8">
-          <h3 class="text-black fs-4 fw-bold text-uppercase">Lukather</h3>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae
-            labore odit magnam in autem nesciunt, amet deserunt
-          </p>
-          <p class="fw-black text-primary fs-3">$299</p>
-          <button type="button" class="btn btn-dark w-100">
-            Agregar al Carrito
-          </button>
-        </div>
-      </div>
+      <Guitarra v-for="guitarra in guitarras" :guitarra="guitarra" />
       <!-- FIN GUITARRA -->
     </div>
   </main>
