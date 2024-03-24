7c4f47af87e2f6c269e8ccb250f46e91a6cfdae2
ISC-EIbarra <EdsonP4100@gmail.com>
2024-03-24 13:10:27 -0700

    Add cart functions

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <!-- Lógica -->
 <script setup>
-import { ref, reactive, onMounted } from 'vue';
+import { ref, reactive, onMounted, watch } from 'vue';
 import { db } from './data/guitarras.js';
 import Guitarra from './components/Guitarra.vue';
 import Header from './components/Header.vue';
@@ -12,10 +12,31 @@ import Footer from './components/Footer.vue';
 
 const guitarras = ref([]);
 const carrito = ref([]);
+const guitarra = ref({});
+
+watch(
+  carrito,
+  () => {
+    guardarLocalStorage();
+  },
+  {
+    deep: true,
+  }
+);
 
 onMounted(() => {
   guitarras.value = db;
+  guitarra.value = db[3];
+
+  const carritoStorage = localStorage.getItem('carrito');
+  if (carritoStorage) {
+    carrito.value = JSON.parse(carritoStorage);
+  }
 });
+
+const guardarLocalStorage = () => {
+  localStorage.setItem('carrito', JSON.stringify(carrito.value));
+};
 
 const agregarCarrito = (guitarra) => {
   const existeCarrito = carrito.value.findIndex(
@@ -29,11 +50,39 @@ const agregarCarrito = (guitarra) => {
     carrito.value.push(guitarra);
   }
 };
+
+const decrementarCantidad = (id) => {
+  const index = carrito.value.findIndex((producto) => producto.id === id);
+  if (carrito.value[index].cantidad <= 1) return;
+  carrito.value[index].cantidad--;
+};
+
+const incrementarCantidad = (id) => {
+  const index = carrito.value.findIndex((producto) => producto.id === id);
+  if (carrito.value[index].cantidad >= 5) return;
+  carrito.value[index].cantidad++;
+};
+
+const eliminarProducto = (id) => {
+  carrito.value = carrito.value.filter((producto) => producto.id !== id);
+};
+
+const vaciarCarrito = () => {
+  carrito.value = [];
+};
 </script>
 
 <!-- HTML -->
 <template>
-  <Header :carrito="carrito" />
+  <Header
+    :carrito="carrito"
+    :guitarra="guitarra"
+    @decrementar-cantidad="decrementarCantidad"
+    @incrementar-cantidad="incrementarCantidad"
+    @agregar-carrito="agregarCarrito"
+    @eliminar-producto="eliminarProducto"
+    @vaciar-carrito="vaciarCarrito"
+  />
   <main class="container-xl mt-5">
     <h2 class="text-center">Nuestra Colección</h2>
 
@@ -52,4 +101,3 @@ const agregarCarrito = (guitarra) => {
 
 <!-- CSS/SCSS -->
 <style scoped></style>
-, onMounted, onMounted
